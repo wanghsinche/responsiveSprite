fe2171144d210b606a65da4eb84af764dd0d72ea
wanghsinche <wanghsinche@hotmail.com>
2017-05-21 20:57:27 +0800

    ..

---
+++ example/images/app.js
@@ -0,0 +1,73 @@
+var fs = require('fs');
+var Images = require('images');
+var imgFilels = null;
+var infoLs = [];
+var css = [];
+var js = [];
+var emptyCanvas = Images(1024, 10240);
+var widthTotal = 0;
+var heightTotal = 0;
+var padding = 0;
+
+let mypromise = new Promise((resolve, reject) => {
+	fs.readdir('./', (err, data) => {
+		if (err) {reject(err)}
+		resolve(data);
+	});
+});
+
+mypromise.then((data) => {
+
+	imgFilels = data.filter(v => /png|jpg/.test(v));
+
+	imgFilels.forEach((v) => {
+		let img = Images(v);
+		infoLs.push({
+			name: v,
+			width: img.width(),
+			height: img.height(),
+			currentY: heightTotal
+		});
+		emptyCanvas.draw(img, 0, heightTotal);
+		if (img.width() > widthTotal) {
+			widthTotal = img.width();
+		}
+		heightTotal += img.height();
+		heightTotal += padding;
+	});
+
+	Images(emptyCanvas, 0, 0, widthTotal, heightTotal).save('_dest.png', {
+		quality: 50
+	});
+
+	infoLs.forEach((v) => {
+		css.push(
+			'.' + v.name.split('.')[0] +
+			'{\n' +
+			'width: ' + v.width + 'rem;\n' +
+			'height: ' + v.height + 'rem;\n' +
+			'background-size: ' + 'auto ' + (heightTotal / v.height * 100).toFixed(2) + '%;\n' +
+			'background-position: ' + '0 ' + (v.currentY / (heightTotal - v.height) * 100).toFixed(2) + '%;\n' +
+			'}'
+		);
+	});
+
+	infoLs.forEach((v)=>{
+		js.push(
+			v.name.split('.')[0]+ '\n' +
+			'top: '+ v.currentY 
+		);
+	});
+
+	console.log(js.join('\n'));
+	// fs.writeFile('info.css', css.join('\n'), (err) => {
+	// 	if (err) {
+	// 		throw err;
+	// 	}
+	// 	console.log('save to info.css');
+	// });
+
+
+}).catch((e) => {
+	throw e;
+});
